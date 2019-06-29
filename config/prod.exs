e9d787d1443cdfb74481cb696e9ffb1ee7976cb8
dorgan <dorgandash@gmail.com>
2019-06-29 02:00:07 -0300

    dont check origin (HACK)

--- config/prod.exs
+++ config/prod.exs
@@ -18,7 +18,8 @@ config :embers, EmbersWeb.Endpoint,
   url: [host: "localhost", port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
-  root: "."
+  root: ".",
+  check_origin: false
 
 # Do not print debug messages in production
 config :logger, level: :info
