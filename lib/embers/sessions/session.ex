e05febe1cacf496e464ce14cccadf132c86ca880
dorgan <dorgandash@gmail.com>
2019-08-09 01:20:25 -0300

    fix dumbfuckery

--- lib/embers/sessions/session.ex
+++ lib/embers/sessions/session.ex
@@ -26,7 +26,7 @@ defmodule Embers.Sessions.Session do
 
   defp set_expires_at(%__MODULE__{} = session, attrs) do
     max_age = attrs[:max_age] || @max_age
-    expires_at = exp_datetime(max_age)
+    {:ok, expires_at} = exp_datetime(max_age)
     %__MODULE__{session | expires_at: DateTime.truncate(expires_at, :second)}
   end
 
