5a943b14a900109651817467fc8f16014f95e5ba
dorgan <dorgandash@gmail.com>
2019-08-25 17:24:49 -0300

    fix previous shit

--- lib/embers_web/controllers/web/session_controller.ex
+++ lib/embers_web/controllers/web/session_controller.ex
@@ -54,7 +54,7 @@ defmodule EmbersWeb.SessionController do
 
   def delete(%Plug.Conn{assigns: %{current_user: %{id: user_id}}} = conn, _params) do
     session_id = get_session(conn, :phauxth_session_id)
-    session_id = if not session_id do
+    session_id = if is_nil(session_id) do
       get_session(conn, :session_id)
     end || session_id
 
