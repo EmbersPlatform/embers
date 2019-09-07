a4472c2a8711fbd2d2d2c3897ae664a63a79027a
dorgan <dorgandash@gmail.com>
2019-09-07 14:33:28 -0300

    Revert "improve metatags"
    This reverts commit c3eb9392c90f5cd9615b2bf95428064ac506b585.

--- lib/embers_web/controllers/web/post_web_controller.ex
+++ lib/embers_web/controllers/web/post_web_controller.ex
@@ -10,17 +10,15 @@ defmodule EmbersWeb.PostWebController do
   def show(%{assigns: %{current_user: nil}} = conn, %{"hash" => hash} = _params) do
     id = IdHasher.decode(hash)
 
-    with {:ok, post} = Posts.get_post(id) do
+    with {:ok, post } = Posts.get_post(id) do
       post = %{
-        post
-        | id: hash,
-          user: %{
-            post.user
-            | meta: %{
-                post.user.meta
-                | avatar: Meta.avatar_map(post.user.meta)
-              }
+        post |
+        id: hash,
+        user: %{
+          post.user | meta: %{
+            post.user.meta | avatar: Meta.avatar_map(post.user.meta)
           }
+        }
       }
 
       og_metatags = build_metatags(post)
@@ -29,54 +27,24 @@ defmodule EmbersWeb.PostWebController do
       |> render("show.html", post: post, og_metatags: og_metatags)
     end
   end
-
   def show(conn, _params) do
     conn
     |> render("index.html")
   end
 
   defp build_metatags(post) do
-    description = post.body
-
-    title =
-      if post.body do
-        "#{post.body} - @#{post.user.username} en Embers"
-      else
-        "Post de @#{post.user.username} en Embers"
-      end
-
-    type =
-      cond do
-        length(post.media) == 1 ->
-          Map.get(List.first(post.media), :type)
-
-        length(post.media) > 0 ->
-          "article"
-
-        true ->
-          "article"
-      end
-
-    medias =
-      for media <- post.media do
-        {media.type, media.url}
-      end
-
-    tags =
-      for tag <- post.tags do
-        tag.name
-      end
-      |> Enum.join(" ")
-
     metatags = [
-      {"site_name", "embers.pw"},
-      {"url", "https://www.embers.pw/post/#{post.id}"},
-      {"title", title},
-      {"description", description},
-      {"type", type},
-      {"tags", tags}
-      | medias
+      {"title", "#{post.body} - @#{post.user.username} en Embers"},
+      {"description", post.body},
+      {"type", "article"}
     ]
+
+    images =
+      for media <- post.media do
+        {"image", media.url}
+      end
+
+    metatags = metatags ++ images
 
     metatags
   end
