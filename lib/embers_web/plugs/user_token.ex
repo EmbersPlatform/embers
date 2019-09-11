184d903c59b3150fecb67634a17c063e8ffc73c9
dorgan <dorgandash@gmail.com>
2019-09-10 21:33:43 -0300

    fix user_token

---
+++ lib/embers_web/plugs/user_token.ex
@@ -0,0 +1,20 @@
+defmodule EmbersWeb.Plugs.UserToken do
+  alias EmbersWeb.Auth.Token
+  alias Embers.Accounts
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user = Pow.Plug.current_user(conn)
+
+    if user do
+      token = Token.sign(%{"user_id" => user.id})
+
+      conn
+      |> assign(:user_token, token)
+    else
+      conn
+    end
+  end
+end
