325cc83053214ae4f0a3f576cba8db3e13014b50
dorgan <dorgandash@gmail.com>
2019-10-30 15:03:55 -0300

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Embers.Mixfile do
   def project do
     [
       app: :embers,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
