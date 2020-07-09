549c7e21f364207c770638fda804f4d4f350237f
doorgan <dorgandash@gmail.com>
2020-07-09 19:48:53 -0300

    rate limit feeds

--- lib/embers_web/controllers/web/feed_controller.ex
+++ lib/embers_web/controllers/web/feed_controller.ex
@@ -12,6 +12,20 @@ defmodule EmbersWeb.FeedController do
 
   plug(:user_check when action in [:timeline, :hide_post])
 
+  plug(
+    :rate_limit_feeds,
+    [max_requests: 20, interval_seconds: 60] when action in [
+        :timeline, :user_statuses, :get_public_feed
+      ]
+  )
+
+  def rate_limit_feeds(conn, options \\ []) do
+    ip = conn.remote_ip
+    ip_string = ip |> :inet_parse.ntoa |> to_string()
+    options = Keyword.merge(options, bucket_name: "feed:#{ip_string}")
+    EmbersWeb.RateLimit.rate_limit(conn, options)
+  end
+
   def timeline(%Plug.Conn{assigns: %{current_user: %{id: user_id}}} = conn, params) do
     results =
       Timeline.get(
