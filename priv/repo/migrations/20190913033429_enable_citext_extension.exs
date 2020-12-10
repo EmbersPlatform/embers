e4980eb6c51b2b280ce29de47a7612d1f0ebb2c4
doorgan <dorgandash@gmail.com>
2020-12-10 20:30:10 +0000

    only create citext extension if it exists

--- priv/repo/migrations/20190913033429_enable_citext_extension.exs
+++ priv/repo/migrations/20190913033429_enable_citext_extension.exs
@@ -2,6 +2,6 @@ defmodule Embers.Repo.Migrations.EnableCitextExtension do
   use Ecto.Migration
 
   def change do
-    execute("CREATE EXTENSION citext", "DROP EXTENSION citext")
+    execute("CREATE EXTENSION IF NOT EXISTS citext", "DROP EXTENSION IF EXISTS citext")
   end
 end
