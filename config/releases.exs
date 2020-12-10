b899d1ee77451727897aee84de2a2f4d434f2e54
doorgan <dorgandash@gmail.com>
2020-12-10 18:18:28 +0000

    add defaults to releases.exs

--- config/releases.exs
+++ config/releases.exs
@@ -32,35 +32,52 @@ config :recaptcha,
   public_key: System.fetch_env!("RECAPTCHA_PUBLIC_KEY"),
   secret: System.fetch_env!("RECAPTCHA_SECRET")
 
-config :ex_aws, :s3, %{
-  access_key_id: System.fetch_env!("S3_ACCESS_KEY"),
-  secret_access_key: System.fetch_env!("S3_SECRET"),
-  scheme: "https://",
-  host: System.fetch_env!("S3_HOST"),
-  region: System.fetch_env!("S3_REGION")
-}
+case System.get_env("EMBERS_STORAGE_DRIVER", "local") do
+  "local" ->
+    config :embers, Embers.FileStorage, store: Embers.FileStorage.Store.Local
 
-config :embers, Embers.FileStorage,
-  store: Embers.FileStorage.Store.S3,
-  bucket: "uploads",
-  schema: "https://",
-  host: System.fetch_env!("S3_HOST_URL")
+  "s3" ->
+    config :ex_aws, :s3, %{
+      access_key_id: System.fetch_env!("S3_ACCESS_KEY"),
+      secret_access_key: System.fetch_env!("S3_SECRET"),
+      scheme: "https://",
+      host: System.fetch_env!("S3_HOST"),
+      region: System.fetch_env!("S3_REGION")
+    }
 
-config :embers, EmbersWeb.Mailer,
-  adapter: Swoosh.Adapters.SMTP,
-  relay: System.fetch_env!("MAIL_RELAY"),
-  username: System.fetch_env!("MAIL_USERNAME"),
-  password: System.fetch_env!("MAIL_PASSWORD"),
-  ssl: false,
-  auth: :always,
-  port: System.fetch_env!("MAIL_SMTP_PORT")
+    config :embers, Embers.FileStorage,
+      store: Embers.FileStorage.Store.S3,
+      bucket: "uploads",
+      schema: "https://",
+      host: System.fetch_env!("S3_HOST_URL")
+end
 
-config :sentry,
-  dsn: System.fetch_env!("SENTRY_DSN"),
-  environment_name: :prod,
-  enable_source_code_context: true,
-  root_source_code_path: File.cwd!(),
-  tags: %{
-    env: "production"
-  },
-  included_environments: [:prod]
+case System.fetch_env!("MAIL_ADAPTER") do
+  "sendgrid" ->
+    config :embers, EmbersWeb.Mailer,
+      adapter: Swoosh.Adapters.Sendgrid,
+      api_key: System.fetch_env!("SENDGRID_API_KEY")
+
+  "smtp" ->
+    config :embers, EmbersWeb.Mailer,
+      adapter: Swoosh.Adapters.SMTP,
+      relay: System.fetch_env!("MAIL_RELAY"),
+      username: System.fetch_env!("MAIL_USERNAME"),
+      password: System.fetch_env!("MAIL_PASSWORD"),
+      ssl: false,
+      auth: :always,
+      port: System.fetch_env!("MAIL_SMTP_PORT")
+end
+
+case System.fetch_env("SENTRY_ENABLE") do
+  {:ok, "true"} ->
+    config :sentry,
+      dsn: System.fetch_env!("SENTRY_DSN"),
+      environment_name: :prod,
+      enable_source_code_context: true,
+      root_source_code_path: File.cwd!(),
+      tags: %{
+        env: "production"
+      },
+      included_environments: [:prod]
+end
